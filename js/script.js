8b7b1c38e863aa6cba2314148000c953729351da
Shermatov Davron Devloper full stack <93197110+DavronShermatov@users.noreply.github.com>
2022-09-10 07:03:49 +0300

    dynamic typing

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// Dinamik malumot turini almashtirish
+
+const num = String(5)
+
+console.log(typeof num);
+
+const book = Number("7")
+
+console.log(typeof book);
+const channelYoutube = 88
+
+const channel = console.log(`sizni youtubeda obunachingiz `+channelYoutube)
+
+const item = console.log(typeof parseInt("88"))
+
+// javasicriptda 0 "" null NaN bu qiymat false qiymatni qaytaradi
+
+const age = 0
+
+if (age){
+    console.log("g")
+}
+const ages = 20
+if (ages){
+    console.log("g")
+}
+
